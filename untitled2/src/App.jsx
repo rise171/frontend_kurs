c1022046a49a78d78b52dc06eb8498af97254cd3
rise171 <krispetrova206@gmail.com>
2025-06-01 11:14:22 +0300

    Initial commit

---
+++ untitled2/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import StartPage from "./Page/StartPage.jsx";
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <StartPage/>
+      </div>
+    </>
+  )
+}
+
+export default App
